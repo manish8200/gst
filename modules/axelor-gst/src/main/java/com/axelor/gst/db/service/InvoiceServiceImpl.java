d992e98b027249152295e3120c75501f92c055e1
manish8200 <golumani169@gmial.com>
2019-07-26 15:52:02 +0530

    invoice,invoice-line updated

--- modules/axelor-gst/src/main/java/com/axelor/gst/db/service/InvoiceServiceImpl.java
+++ modules/axelor-gst/src/main/java/com/axelor/gst/db/service/InvoiceServiceImpl.java
@@ -1,12 +1,14 @@
 package com.axelor.gst.db.service;
 
-import java.util.ArrayList;
+import java.math.BigDecimal;
 import java.util.Collection;
-import java.util.List;
-
+import com.axelor.gst.db.Address;
+import com.axelor.gst.db.Company;
 import com.axelor.gst.db.Contact;
 import com.axelor.gst.db.Invoice;
+import com.axelor.gst.db.Invoice_line;
 import com.axelor.gst.db.Party;
+import com.axelor.gst.db.State;
 
 public class InvoiceServiceImpl implements InvoiceService {
 
@@ -14,16 +16,17 @@ public class InvoiceServiceImpl implements InvoiceService {
 	public Contact setcontact(Invoice invoice) {
 		// TODO Auto-generated method stub
 
-		Party party = invoice.getPartys();
+		Party party = invoice.getParty();
 
 		Collection<Contact> partyContact = party.getContact();
 		Contact contact = null;
-		String i = null;
+		System.out.println(contact);
 
 		for (Contact contacts : partyContact) {
-			if (contacts.getType().equals("Primary")) {
-				contacts.getName();
+			if (contacts.getType().equals("primary")) {
+
 				contact = contacts;
+				// System.out.println(contact);
 			}
 		}
 
@@ -31,4 +34,107 @@ public class InvoiceServiceImpl implements InvoiceService {
 
 	}
 
+	@Override
+	public Address setaddress(Invoice invoices) {
+		// TODO Auto-generated method stub
+
+		Party party = invoices.getParty();
+
+		Collection<Address> partyAddress = party.getAddress();
+		Address address = null;
+
+		for (Address addresses : partyAddress) {
+			if (addresses.getType().equals("invoice")) {
+
+				address = addresses;
+				// System.out.println(address);
+			}
+		}
+
+		return address;
+	}
+
+	@Override
+	public Address setshipping(Invoice shipping) {
+		// TODO Auto-generated method stub
+		Party party = shipping.getParty();
+
+		Collection<Address> partyAddress = party.getAddress();
+		Address address = null;
+
+		for (Address addresses : partyAddress) {
+			if (addresses.getType().equals("shipping")) {
+				address = addresses;
+				// System.out.println(address);
+			}
+		}
+		return address;
+	}
+
+	@Override
+	public Address setinvoiceasship(Invoice shipping) {
+		// TODO Auto-generated method stub
+		Party party = shipping.getParty();
+
+		Boolean myaddress;
+		myaddress = shipping.getUseInvoiceAddress();
+
+		Collection<Address> partyAddress = party.getAddress();
+
+		Address inaddress;
+		Address useaddress = null;
+
+		for (Address addresses : partyAddress) {
+			if (myaddress) {
+				if (addresses.getType().equals("invoice")) {
+					inaddress = addresses;
+					return inaddress;
+				}
+			} else if (addresses.getType().equals("shipping")) {
+				System.out.println(addresses);
+				useaddress = addresses;
+				return useaddress;
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public BigDecimal setvalues(Invoice invoiceLine) {
+		// TODO Auto-generated method stub
+		BigDecimal netamount = invoiceLine.getNetAmount();
+		return netamount;
+	}
+
+	@Override
+	public Invoice_line validateaddress(Invoice invoice, Invoice_line inline) {
+		// TODO Auto-generated method stub
+		Address addresses = invoice.getInvoiceAddress();
+		State states = addresses.getState();
+		Company company = invoice.getCompanies();
+		Address address = company.getAddress();
+		State addressstate = address.getState();
+		BigDecimal gst = null, Sgst = null, Igst = null, netamount, price = null, gstRate, grossamount;
+		Integer qty = null;
+		int divide = 2;
+		gst = inline.getGstRate();
+		qty = inline.getQty();
+		price = inline.getPrice();
+		netamount = price.multiply(new BigDecimal(qty));
+		gstRate = gst.divide(new BigDecimal(divide));
+
+		if (addressstate.equals(states)) {
+			Sgst = netamount.multiply(gstRate);
+			grossamount = netamount.add(Sgst);
+		} else {
+			Igst = netamount.multiply(gst);
+			grossamount = netamount.add(Igst);
+		}
+		inline.setNetAmount(netamount);
+		inline.setSGST(Sgst);
+		inline.setCGST(Sgst);
+		inline.setIGST(Igst);
+		inline.setGrossAmount(grossamount);
+		return inline;
+	}
 }
--- modules/axelor-gst/src/main/java/com/axelor/gst/db/web/GstController.java
+++ modules/axelor-gst/src/main/java/com/axelor/gst/db/web/GstController.java
@@ -1,7 +1,7 @@
 package com.axelor.gst.db.web;
 
 import java.math.BigDecimal;
-
+import java.util.Collection;
 import com.axelor.db.JpaSupport;
 import com.axelor.gst.db.Address;
 import com.axelor.gst.db.Contact;
@@ -37,80 +37,85 @@ public class GstController extends JpaSupport {
 
 		{
 			BigDecimal cgst;
-			cgst = product1.getGst_rate();
+			cgst = product1.getGstRate();
 
-			rsp.setValue("gst_ratte", cgst);
+			rsp.setValue("gstRate", cgst);
 		}
-	}
-
-	public void setNetAmount(ActionRequest rq, ActionResponse rsp) {
-
-		Invoice_line inline = rq.getContext().asType(Invoice_line.class);
-
-		Integer qty;
-		BigDecimal price, gst, Igst, gst_rate;
-
-		gst_rate = inline.getGst_ratte();
-		qty = inline.getQty();
-		price = inline.getPrice();
-		gst = price.multiply(new BigDecimal(qty));
-		Igst = gst.multiply(gst_rate);
-		rsp.setValue("net_amount", gst);
-		rsp.setValue("IGST", Igst);
-
-	}
-	/*
-	 * public void setIgst(ActionRequest rq , ActionResponse rsp) {
-	 * 
-	 * Invoice_line inline = rq.getContext().asType(Invoice_line.class); BigDecimal
-	 * a,b,c; a= inline.getNet_amount(); b= inline.getGst_ratte();
-	 * 
-	 * c = a.multiply(b);
-	 * 
-	 * rsp.setValue("IGST", c); }
-	 */
-
-	public void setSgst(ActionRequest rq, ActionResponse rsp) {
-		Invoice_line inline = rq.getContext().asType(Invoice_line.class);
-
-		BigDecimal net_amount, gst_rate, gst, Cgst, gross_amount;
-
-		int divide = 2;
-
-		net_amount = inline.getNet_amount();
-		gst_rate = inline.getGst_ratte();
-		gst = gst_rate.divide(new BigDecimal(divide));
-		Cgst = net_amount.multiply(gst);
-		gross_amount = net_amount.add(Cgst);
-
-		rsp.setValue("SGST", Cgst);
-		rsp.setValue("CGST", Cgst);
-		rsp.setValue("gross_amount", gross_amount);
 	}
 
 	public void setaddress(ActionRequest rq, ActionResponse rsp) {
 
 		Address address = rq.getContext().asType(Address.class);
 
-		String a, d;
-		String c;
-		String b;
-
-		a = address.getLine1();
-		b = address.getCity().getName();
-		c = address.getState().getName();
-
-		d = a + " " + b + " " + c;
-
-		rsp.setValue("full_addresss", d);
+		String line1, fulladdress;
+		String state;
+		String city;
+		line1 = address.getLine1();
+		city = address.getCity().getName();
+		state = address.getState().getName();
+		fulladdress = line1 + " " + city + " " + state;
+		rsp.setValue("fullAddresss", fulladdress);
 	}
 
 	public void setInvoice(ActionRequest rq, ActionResponse rsp) {
 		Invoice invoice = rq.getContext().asType(Invoice.class);
-
 		Contact contact = inservice.setcontact(invoice);
+		rsp.setValue("partyContact", contact);
+	}
 
-		rsp.setValue("party_contact", contact.getName());
+	public void setInvoiceAddress(ActionRequest rq, ActionResponse rsp) {
+		Invoice invoices = rq.getContext().asType(Invoice.class);
+		Address address = inservice.setaddress(invoices);
+		// System.out.println(address);
+		rsp.setValue("invoiceAddress", address);
+	}
+
+	public void setShippingaddress(ActionRequest rq, ActionResponse rsp) {
+		Invoice invoice = rq.getContext().asType(Invoice.class);
+		Address addresses = inservice.setshipping(invoice);
+		rsp.setValue("shippingAddress", addresses);
+	}
+
+	public void setInvoice_as_shipping(ActionRequest rq, ActionResponse rsp) {
+
+		Invoice invoice = rq.getContext().asType(Invoice.class);
+		Address address = inservice.setinvoiceasship(invoice);
+		rsp.setValue("shippingAddress", address);
+	}
+
+	public void setInvoiceAmount(ActionRequest rq, ActionResponse rsp) {
+
+		Invoice invoice = rq.getContext().asType(Invoice.class);
+		BigDecimal netamount;
+		BigDecimal netIgst, netCgst, netSgst, grossAmount;
+
+		Collection<Invoice_line> inline = invoice.getInvoiceItems();
+		for (Invoice_line invoice_line : inline) {
+			netamount = invoice_line.getNetAmount();
+			netIgst = invoice_line.getIGST();
+			netCgst = invoice_line.getCGST();
+			netSgst = invoice_line.getSGST();
+			grossAmount = invoice_line.getSGST();
+			// System.out.println(netIgst);
+			rsp.setValue("netAmount", netamount);
+			rsp.setValue("netIGST", netIgst);
+			rsp.setValue("netCSGT", netCgst);
+			rsp.setValue("netSGST", netSgst);
+			rsp.setValue("grossAmount", grossAmount);
+		}
+	}
+
+	public void setValidation(ActionRequest rq, ActionResponse rsp) {
+		Invoice_line inline = rq.getContext().asType(Invoice_line.class);
+		Invoice invoice = rq.getContext().getParent().asType(Invoice.class);
+		Invoice_line invoice_line = inservice.validateaddress(invoice, inline);
+		System.out.println(invoice_line);
+		rsp.setValue("netAmount", invoice_line.getNetAmount());
+		rsp.setValue("IGST", invoice_line.getIGST());
+		rsp.setValue("SGST", invoice_line.getSGST());
+		rsp.setValue("CGST", invoice_line.getCGST());
+		rsp.setValue("grossAmount", invoice_line.getGrossAmount());
 
 	}
+
 }
